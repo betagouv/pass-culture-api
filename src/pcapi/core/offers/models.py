33730ff4e69f8a2761a888cb1cd511613fda15d8
Adrien SAUNIER <adrien.saunier@passculture.app>
2021-04-30 16:16:16 +0200

    (PC-8243): First step of migration

--- src/pcapi/core/offers/models.py
+++ src/pcapi/core/offers/models.py
@@ -323,6 +323,15 @@ class Offer(PcObject, Model, ExtraDataMixin, DeactivableMixin, ProvidableMixin, 
         server_default="APPROVED",
     )
 
+    idAtProvider = Column(
+        String(70),
+        CheckConstraint(
+            '"lastProviderId" IS NULL OR "idAtProvider" IS NOT NULL',
+            name="check_providable_with_provider_has_idatprovider",
+        ),
+        nullable=True,
+    )
+
     @hybrid_property
     def isSoldOut(self):
         for stock in self.stocks:
